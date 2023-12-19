8fe082d126a9819344f00199752d20f7031abe76
kintela <rquintela@fulcrum.es>
2023-12-19 12:38:03 +0100

    ya me conecto a la api de spotify pero me pide siempre aceptar

--- myWeb/src/app/spotify/spotify.component.ts
+++ myWeb/src/app/spotify/spotify.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from '../services/spotify.service';
 import { switchMap, take } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-spotify',
@@ -9,36 +10,52 @@ import { switchMap, take } from 'rxjs';
 })
 export class SpotifyComponent implements OnInit {
   clientId = "b517fa4fb32742c28cf6c76956591e87";
-  code = undefined;
+  code: string | null = null;
+  displayName!:string;
+  id!:string;
+  email!:string;
+  spotifyUri!:string;
+  spotifyUrl!:string
+  profileImg!:string;
 
-  constructor(private spotifyService:SpotifyService) { }
+  constructor(private spotifyService:SpotifyService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    if(!this.code){
-      this.spotifyService.redirectToAuthCodeFlow(this.clientId)
-      .pipe(take(1))
-      .subscribe(authUrl => {
-        window.location.href = authUrl;
-      });
-    }
-    else {
-      this.spotifyService.getAccessToken(this.clientId, this.code)
-        .pipe(
-          switchMap(accessToken => this.spotifyService.fetchProfile(accessToken))
-        )
-        .subscribe(
-          profile => {
-            this.populateUI(profile);
-          },
-          error => {
-            console.error(error);
-          }
-        );
-    }
+    this.route.queryParams.subscribe(params => {
+      this.code = params['code']; // Esto captura el código de la URL
+      console.log("Captured code:", this.code);
+
+      if (this.code) {
+        this.spotifyService.getAccessToken(this.clientId, this.code)
+          .pipe(
+            switchMap(accessToken => this.spotifyService.fetchProfile(accessToken))
+          )
+          .subscribe(
+            profile => {
+              this.populateUI(profile);
+            },
+            error => {
+              console.error(error);
+            }
+          );
+      } else {
+        this.spotifyService.redirectToAuthCodeFlow(this.clientId)
+          .pipe(take(1))
+          .subscribe(authUrl => {
+            window.location.href = authUrl;
+          });
+      }
+    });
   }
 
   populateUI(profile: any) {
-    console.log(profile);
+    console.log('populateUI',profile);
+    this.displayName = profile.display_name;
+    this.id = profile.id;
+    this.email = profile.email;
+    this.spotifyUri = profile.uri;
+    this.spotifyUrl = profile.external_urls.spotify;
+    this.profileImg = profile.images[0].url;
   }
 
 }

e344a575ccf4c4b2d8dfe03f633e3c19570151ea
FULCRUM\RQUINTELA <rquintela@fulcrum.es>
2023-12-21 16:22:23 +0100

    cargar el proyecto de azure functions

---
+++ azurefunctions/getApiYoutube.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace Kintela.Function
+{
+    public static class GetApiYoutube
+    {
+        [FunctionName("GetApiYoutube")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+             string keyVaultName = Environment.GetEnvironmentVariable("KEY_VAULT_NAME");
+            var kvUri = $"https://{keyVaultName}.vault.azure.net";
+
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+            try
+            {
+                KeyVaultSecret secret = await client.GetSecretAsync("YoutubeApi");
+                string apiKey = secret.Value;
+
+                // Aquí devolvemos la clave API en la respuesta, pero podría ser un riesgo de seguridad.
+                // Normalmente, usarías la clave API para realizar alguna operación y luego devolverías el resultado de esa operación.
+                return new OkObjectResult(new { ApiKey = apiKey });
+            }
+            catch (Exception e)
+            {
+                log.LogError($"Error al obtener el secreto del Key Vault: {e.Message}");
+                return new StatusCodeResult(500); // Error interno del servidor
+            }
+        }
+    }
+}
---
+++ azurefunctions/getLyricsMusixMatch.cs
@@ -0,0 +1,67 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System.Net.Http;
+using System.Net;
+
+namespace Kintela.Function
+{
+    public static class GetLyricsMusixMatch
+    {
+        [FunctionName("GetLyricsMusixMatch")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            // Obtener los parámetros de la solicitud
+            string track_id = req.Query["track_id"];
+            // Verificar si los parámetros son válidos
+            if (string.IsNullOrEmpty(track_id) || string.IsNullOrEmpty(track_id))
+            {
+                return new BadRequestObjectResult("Please provide a track_id parameter.");
+            }
+            // Obtener la clave API de Musixmatch desde las variables de entorno
+            string apiKey = Environment.GetEnvironmentVariable("musixmatchapikey", EnvironmentVariableTarget.Process);
+            // Crear una instancia de HttpClient
+            using (var client = new HttpClient())
+            {
+                // Construir la URL de la API de Musixmatch
+                string baseUrl = "https://api.musixmatch.com/ws/1.1/track.lyrics.get";
+                string url = $"{baseUrl}?track_id={WebUtility.UrlEncode(track_id)}&apikey={apiKey}";
+                try
+                {
+                    // Hacer la llamada a la API y obtener la respuesta
+                    var response = await client.GetAsync(url);
+                    // Asegurarse de que la respuesta es exitosa
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Leer el contenido de la respuesta
+                        string responseContent = await response.Content.ReadAsStringAsync();
+                        // Devolver el contenido de la respuesta
+                        return new OkObjectResult(responseContent);
+                    }
+                    else
+                    {
+                        // Manejar errores, como un código de estado HTTP no exitoso
+                        log.LogError($"Error en la llamada a la API: {response.StatusCode}");
+                        return new StatusCodeResult((int)response.StatusCode);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Manejar excepciones, como problemas de red
+                    log.LogError($"Excepción al llamar a la API: {ex.Message}");
+                    return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                }
+            }
+        }        
+    }    
+}

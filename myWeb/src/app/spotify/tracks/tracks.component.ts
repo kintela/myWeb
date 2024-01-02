bac859ca39619f90c214bcaeda7172feaac3d158
FULCRUM\RQUINTELA <rquintela@fulcrum.es>
2024-01-02 15:15:56 +0100

    Ya no da error si no encuentra las lyrtics

--- myWeb/src/app/spotify/tracks/tracks.component.ts
+++ myWeb/src/app/spotify/tracks/tracks.component.ts
@@ -45,7 +45,7 @@ export class TracksComponent implements OnInit{
     });
   }
 
-  onSelectTrack(track: any): void {
+  onSelectTrackOLD(track: any): void {
     this.pistaSeleccionada = track.name;
     this.artistaSeleccionado = track.artists[0].name;
     this.albumSeleccionado = track.album.name;
@@ -82,6 +82,59 @@ export class TracksComponent implements OnInit{
       }
     );
   }
+
+  onSelectTrack(track: any): void {
+    this.pistaSeleccionada = track.name;
+    this.artistaSeleccionado = track.artists[0].name;
+    this.albumSeleccionado = track.album.name;
+  
+    // Llamada a MusixMatch para obtener el ID de la pista
+    this.musixMatchService.getMusixMatchTrackId(track.artists[0].name, track.name).pipe(
+      switchMap(response => {
+        // Verifica si hay track_list y si tiene elementos
+        if (response.message.body.track_list && response.message.body.track_list.length > 0) {
+          const trackId = response.message.body.track_list[0].track.track_id;
+          console.log('Track ID:', trackId);
+  
+          // Obtiene las letras de la canción
+          return this.musixMatchService.getMusixMatchTrackLyrics(trackId);
+        } else {
+          // Si no hay pistas, emite un error o un valor que indique la ausencia de letras
+          throw new Error('Track not found in MusixMatch');
+        }
+      }),
+      switchMap(lyricsResponse => {
+        // Verifica si el cuerpo de la letra está disponible
+        if (lyricsResponse.message.body.lyrics) {
+          this.letrasCancion = lyricsResponse.message.body.lyrics.lyrics_body;
+        } else {
+          // Si no hay letras, establece un mensaje predeterminado
+          this.letrasCancion = 'Letras no disponibles.';
+        }
+  
+        // Formar el término de búsqueda para YouTube
+        const searchTerm = `${this.artistaSeleccionado} ${this.albumSeleccionado} ${this.pistaSeleccionada}`;
+  
+        // Llamada a la API de YouTube para obtener el video
+        return this.youtubeService.getVideos(searchTerm);
+      })
+    ).subscribe(
+      videoResponse => {
+        console.log('Video:', videoResponse);
+        if (videoResponse.items && videoResponse.items.length > 0) {
+          this.setVideo('https://www.youtube.com/embed/' + videoResponse.items[0].id.videoId);
+        } else {
+          console.log('No se encontraron videos para esta pista.');
+        }
+      },
+      error => {
+        console.error('Error al obtener datos:', error);
+        this.letrasCancion = 'Letras no disponibles.';
+        // Puedes también establecer aquí un video predeterminado o una imagen estática.
+      }
+    );
+  }
+  
   
 
   private setVideo(url: string): void {

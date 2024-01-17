436bffa0ad65eb23643b329a34ea65ab36538e06
FULCRUM\RQUINTELA <rquintela@fulcrum.es>
2024-01-17 09:46:49 +0100

    Iniciar historico Discos
    He empezado con la trayectoria de Tom Petty en solitario, con los heartbreakers y los travellin Wilburys

---
+++ myWeb/src/app/data/IHistoricoDiscos.ts
@@ -0,0 +1,10 @@
+export interface IHistoricoDiscos{
+    titulo:string;
+    estudio?:string;
+    year:number;    
+    grupo:string;
+    spotify?:string;
+    caratula?:string;
+    productor?:string;
+    discografica?:string;
+}

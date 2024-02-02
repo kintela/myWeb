0f43fa1cbb38c54352f6fe54551a32f7c21805cb
kintela <rquintela@fulcrum.es>
2024-02-02 13:39:14 +0100

    lista de la compra
    Guardar en fotmato json

--- myWeb/src/app/planificador-de-menus/lista-compra/lista-compra.component.ts
+++ myWeb/src/app/planificador-de-menus/lista-compra/lista-compra.component.ts
@@ -16,4 +16,18 @@ export class ListaCompraComponent {
     this.listaCompra.splice(index, 1);
   }
   
+  guardarListaCompra() {
+    const dataStr = JSON.stringify(this.listaCompra);
+    const dataUri = 'data:application/json;charset=utf-8,' + encodeURIComponent(dataStr);
+  
+    const date = new Date();
+    const formattedDate = date.toISOString().split('T')[0]; // Formato: 'aaaa-mm-dd'
+
+    const exportFileDefaultName = `lista_compra-${formattedDate}.json`;
+  
+    const linkElement = document.createElement('a');
+    linkElement.setAttribute('href', dataUri);
+    linkElement.setAttribute('download', exportFileDefaultName);
+    linkElement.click();
+  }
 }

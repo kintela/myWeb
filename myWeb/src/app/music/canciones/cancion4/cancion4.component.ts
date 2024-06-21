e095b23f3b62c1ba894abdbb08af3fcd3fddd762
kintela <rquintela@fulcrum.es>
2024-06-21 21:16:44 +0200

    subir cancion

--- myWeb/src/app/music/canciones/cancion4/cancion4.component.ts
+++ myWeb/src/app/music/canciones/cancion4/cancion4.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { MatDialog } from '@angular/material/dialog';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { ReproductorVideoComponent } from 'src/app/shared/reproductor-video/reproductor-video.component';
+import { VisorImagenComponent } from 'src/app/shared/visor-imagen/visor-imagen.component';
 
 @Component({
-  selector: 'app-cancion4',
+  selector: 'app-cancion5',
   templateUrl: './cancion4.component.html',
   styleUrls: ['./cancion4.component.scss']
 })
@@ -12,23 +15,34 @@ export class Cancion4Component implements OnInit {
   grupo:string;
   album:string;
   anio:number;
+  currentVideoUrl: SafeResourceUrl;
 
-  displayedColumns: string[] = ['video1', 'video2'];
-  videos = [
-    { video1: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/k_ZDyiue-aU'), video2: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/YTsDs4K5Kkk') },
-    { video1: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/8Ix-cJZv5vc'), video2: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/MZFhFd3pxxs') },
-    { video1: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/8SYQY3Sa014'), video2: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/rYFSKEDEeEc') },
-    { video1: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/sF3SZBGrX3c'), video2: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/I952VR0sGfE') },
-  ];
- constructor(private sanitizer: DomSanitizer, private route: ActivatedRoute) { }
+ constructor(private sanitizer: DomSanitizer, private route: ActivatedRoute, private dialog: MatDialog) { }
 
- ngOnInit(): void {
-  this.route.queryParams.subscribe(params => {
-    this.nombreCancion = params['titulo'];
-    this.grupo = params['grupo'];
-    this.album = params['album'];
-    this.anio = params['anio'];
-  });
-}
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.nombreCancion = params['titulo'];
+      this.grupo = params['grupo'];
+      this.album = params['album'];
+      this.anio = params['anio'];
+    });
+  }
+
+
+  
+  mostrarVideo(videoUrl: string) {
+    this.dialog.open(ReproductorVideoComponent, {
+      data: {
+        url: videoUrl
+      }
+    });
+  }
+
+  openImageViewer(image: string) {
+    this.dialog.open(VisorImagenComponent, {
+      data: { image: image },
+      panelClass: 'custom-dialog-container' // Clase para estilos personalizados
+    });
+  }  
 
 }

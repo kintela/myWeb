066413d7aca425e12190eb4f16103b3e2d8d190b
PortatilCasa\kintela <rquintela@fulcrum.es>
2025-04-03 09:58:21 +0200

    para publicar

--- myWeb/src/app/services/comun.service.ts
+++ myWeb/src/app/services/comun.service.ts
@@ -5,8 +5,8 @@ import { CategoriaDTO } from '../data/DTOs/categoriaDTO';
   providedIn: 'root'
 })
 export class ComunService {
-  urlWebApi = 'https://localhost:7228/api';
-  //urlWebApi = 'https://api.kintela.es/api';
+  //urlWebApi = 'https://localhost:7228/api';
+  urlWebApi = 'https://api.kintela.es/api';
   //urlWebApi = 'https://kintelaapi-e0axhtfzdzdycqec.northeurope-01.azurewebsites.net/api';
   
 
